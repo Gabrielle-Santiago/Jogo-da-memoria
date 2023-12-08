59071ae8a93efb4f7982ba5def2688750124bcc8
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2023-12-08 10:02:47 -0300

    Finalização do jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,4 @@
-const emojis = [
+let emojis = [
     "❤️","❤️",
     "🔥","🔥",
     "⭐","⭐",
@@ -11,13 +11,49 @@ const emojis = [
 
 let openCards = [];
 
-// O método sort() ordena os elementos do próprio array e retorna o array. A ordenação não é necessariamente estável.
+        const shuffleEmojis = emojis.sort(() => Math.random() > 0.5 ? 2 : -1);
 
-let shuffleEmojis = emojis.sort(() => (Math.random() > Math.random() > 0.5 ? 2 : -1));
+        const gameContainer = document.querySelector('.game');
 
-for (let i = 0; i < emojis.length; i++) {
-    let box = document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffleEmojis;
-    document.querySelector(".game").appendChild(box)
-}
+        shuffleEmojis.forEach(emoji => {
+            const box = document.createElement("div");
+            box.className = "item"; 
+            // Use className para definir a classe
+
+            // Adiciona o emoji ao conteúdo da caixa
+            box.textContent = emoji;
+
+            // Adiciona a caixa ao contêiner do jogo
+            gameContainer.appendChild(box);
+
+            // Convenção comum para indicar uma função que lida com eventos de clique em elementos.
+            box.onclick = handleClick;
+        });
+
+        function handleClick() {
+            if (openCards.length < 2) {
+                this.classList.add("boxOpen");
+                openCards.push(this);
+            }
+
+            if(openCards.length == 2){
+                setTimeout(checkMatch, 500);
+            }
+        }
+
+        function checkMatch() {
+            if(openCards[0].innerHTML === openCards[1].innerHTML) {
+
+                openCards[0].classList.add("boxMatch");
+                openCards[1].classList.add("boxMatch");
+                
+            } else {
+                openCards[0].classList.remove("boxOpen");
+                openCards[1].classList.remove("boxOpen");
+            }
+            openCards = [];
+            
+            if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+                alert("Você venceu!")
+            }
+        }
